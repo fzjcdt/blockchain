df3576bb638e10e4a28cf23e9133191bc390a276
fzjcdt <11510785@mail.sustc.edu.cn>
2018-05-01 23:09:17 +0800

    add rocksdb utils

--- src/main/java/store/RocksDBUtil.java
+++ src/main/java/store/RocksDBUtil.java
@@ -1,6 +1,12 @@
 package store;
 
+import block.Block;
 import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import util.SerializeUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RocksDBUtil {
     private static final String DB_FILE = "blockchain.db";
@@ -21,4 +27,69 @@ public class RocksDBUtil {
     }
 
     private RocksDB db;
+    private Map<String, byte[]> blocksBucket;
+
+    private RocksDBUtil() {
+        openDB();
+        initBlockBucket();
+    }
+
+    private void openDB() {
+        try {
+            db = RocksDB.open(DB_FILE);
+
+        } catch (RocksDBException e) {
+            throw new RuntimeException("Fail to open rocks db", e);
+        }
+    }
+
+    private void initBlockBucket() {
+        try {
+            byte[] blockBucketKey = SerializeUtil.serialize(BLOCKS_BUCKET_KEY);
+            byte[] blockBucketBytes = db.get(blockBucketKey);
+            if (blockBucketBytes != null) {
+                blocksBucket = new HashMap<String, byte[]>();
+            }
+
+        } catch (RocksDBException e) {
+            throw new RuntimeException("Fail to init block bucket", e);
+        }
+    }
+
+    public void putLastBlockHash(String tipBlockHash) {
+        try {
+            blocksBucket.put(LAST_BLOCK_KEY, SerializeUtil.serialize(tipBlockHash));
+            db.put(SerializeUtil.serialize(BLOCKS_BUCKET_KEY), SerializeUtil.serialize(blocksBucket));
+        } catch (RocksDBException e) {
+            throw new RuntimeException("Fail to put last block hash", e);
+        }
+    }
+
+    public String getLastBlockHash() {
+        byte[] lastBlockHashBytes = blocksBucket.get(LAST_BLOCK_KEY);
+        return lastBlockHashBytes == null ? "" : (String) SerializeUtil.deserialize(lastBlockHashBytes);
+    }
+
+    public void putBlock(Block block) {
+        try {
+            blocksBucket.put(block.getHash(), SerializeUtil.serialize(block));
+            db.put(SerializeUtil.serialize(BLOCKS_BUCKET_KEY), SerializeUtil.serialize(blocksBucket));
+
+        } catch (RocksDBException e) {
+            throw new RuntimeException("Fail to put block", e);
+        }
+    }
+
+    public Block getBlock(String blockHash) {
+        return (Block) SerializeUtil.deserialize(blocksBucket.get(blockHash));
+    }
+
+    public void closeDB() {
+        try {
+            db.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Fail to close db", e);
+        }
+    }
+
 }
