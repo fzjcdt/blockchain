1e6f106a4e4742779ec73c8d1c98191ecc61be84
fzjcdt <11510785@mail.sustc.edu.cn>
2018-05-05 23:00:13 +0800

    improve test input

--- src/main/java/test/BlockChainTest.java
+++ src/main/java/test/BlockChainTest.java
@@ -6,22 +6,32 @@ import network.P2P;
 import java.util.Scanner;
 
 public class BlockChainTest {
+    static Scanner input = new Scanner(System.in);
+
+    public static String readData() {
+        String rst = input.nextLine();
+        while (rst == "\n" || rst.replace(" ", "").equals("")) {
+            rst = input.nextLine();
+        }
+
+        return rst;
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         BlockChain blockChain = BlockChain.newBlockChain();
         int type = 0;
         String data = "";
         P2P.getInstance();
 
         while (true) {
-            System.out.println("quit: -1, add block: 0, print: 1");
+            System.out.println("*** quit:-1, add block:0, print:1 ***");
             type = input.nextInt();
             if (type == -1) {
                 break;
             }
             switch (type) {
                 case 0:
-                    data = input.next();
+                    data = readData();
                     blockChain.addBlock(data);
                     break;
                 case 1:
