636ed7a0c973c797c246a281107fed5e45810a6f
fzjcdt <11510785@mail.sustc.edu.cn>
2018-05-30 23:52:09 +0800

    improve UTXO

--- src/main/java/transaction/Transaction.java
+++ src/main/java/transaction/Transaction.java
@@ -1,5 +1,8 @@
 package transaction;
 
+import util.Sha256Util;
+import wallet.SignatureUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +24,72 @@ public class Transaction {
         outputs = new ArrayList<TXOutput>();
     }
 
-    public double getInputsValue() {
-        double total = 0.0;
+    public boolean processTransaction() {
+        try {
+            if (!verifySignature()) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
 
         for (TXInput input : inputs) {
-            // TO DO
+            input.setUTXO(UTXOSet.UTXOs.get(input.getOutputId()));
+        }
+
+        double leftOver = getInputsValue() - value;
+        transactionId = calulateHash();
+        outputs.add(new TXOutput(this.receiver, value, transactionId));
+        if (leftOver > 0) {
+            outputs.add(new TXOutput(this.sender, leftOver, transactionId));
+        }
+
+        for (TXOutput output : outputs) {
+            UTXOSet.UTXOs.put(output.getId(), output);
+        }
+
+        for (TXInput input : inputs) {
+            if (input.getUTXO() == null) {
+                continue;
+            }
+
+            UTXOSet.UTXOs.remove(input.getUTXO().getId());
+        }
+
+        return true;
+    }
+
+    private String calulateHash() {
+        return Sha256Util.sha256Encryption(sender + receiver + Double.toString(value));
+    }
+
+    public void generateSignature(String privateKey) {
+        try {
+            this.signature = SignatureUtil.applySignature(privateKey,
+                    sender + receiver + Double.toString(value));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean verifySignature() throws Exception {
+        String data = sender + receiver + Double.toString(value);
+        return SignatureUtil.verifySignature(sender, data, signature);
+    }
+
+    public double getInputsValue() {
+        double total = 0.0;
+        for (TXInput input : inputs) {
+            total += input.getUTXO().getValue();
+        }
+
+        return total;
+    }
+
+    public double getOutputValue() {
+        double total = 0.0;
+        for (TXOutput output : outputs) {
+            total += output.getValue();
         }
 
         return total;
