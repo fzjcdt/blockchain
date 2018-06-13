fc554d09ab3cffc832cb2c0798c6930ae7cd0912
fzjcdt <11510785@mail.sustc.edu.cn>
2018-06-13 10:39:31 +0800

    improve serialize util

--- src/main/java/util/SerializeUtil.java
+++ src/main/java/util/SerializeUtil.java
@@ -3,28 +3,29 @@ package util;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-import log.LogUtil;
-
-import java.util.logging.Level;
 
 public class SerializeUtil {
 
+    public final static Kryo kryo = new Kryo();
+
+    static {
+        kryo.setRegistrationRequired(false);
+    }
+
     public static Object deserialize(byte[] bytes) {
         Input input = new Input(bytes);
-        Object rst = new Kryo().readClassAndObject(input);
+        Object rst = kryo.readClassAndObject(input);
         input.close();
 
-        LogUtil.Log(Level.INFO, "Deserialize");
         return rst;
     }
 
     public static byte[] serialize(Object obj) {
         Output output = new Output(409600, -1);
-        new Kryo().writeClassAndObject(output, obj);
+        kryo.writeClassAndObject(output, obj);
         byte[] bytes = output.toBytes();
         output.close();
 
-        LogUtil.Log(Level.INFO, "Serialize");
         return bytes;
     }
 }
